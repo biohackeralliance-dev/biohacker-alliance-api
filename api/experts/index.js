f4232ca0d385e0b7768f771b9661fc433570d3f8
biohackeralliance-dev <biohackeralliance@gmail.com>
2026-02-15 20:23:32 +0100

    Update index.js

--- api/experts/index.js
+++ api/experts/index.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     return res.status(200).end()
   }
 
-  if (req.method !== 'GET') {
+  if (req.method !== 'GET') { 
     return res.status(405).json({ error: 'Method not allowed' })
   }
 
